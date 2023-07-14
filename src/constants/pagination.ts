ed4172d242b3dc5369f7b61c28f080bed52a917d
Ruman-Islam <rumanislam48@gmail.com>
2023-07-15 03:08:05 +0600

    enum setup done'

---
+++ src/constants/pagination.ts
@@ -0,0 +1,6 @@
+/**
+ * paginationFields is an array that defines the valid fields for pagination options.
+ * It specifies the allowed query parameters for pagination, including page, limit, sortBy, and sortOrder.
+ * These fields can be used to control the pagination behavior when retrieving data from the database.
+ */
+export const paginationFields = ['page', 'limit', 'sortBy', 'sortOrder'];
--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,59 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
+import { Server } from 'http';
+
+// Handling uncaught exceptions
+/**
+ The uncaughtException event in Node.js is emitted when an unhandled exception occurs in the application. It provides a way to catch and handle exceptions that would otherwise cause the Node.js process to terminate abruptly.
+ Using this mechanism allows you to catch unhandled exceptions and perform necessary actions, such as logging the error, performing cleanup tasks, or gracefully shutting down the application before it crashes. It helps in preventing the process from hanging or becoming unresponsive due to an unhandled exception.
+ */
+process.on('uncaughtException', error => {
+  // errorLogger.error(error);
+  console.log(error);
+  process.exit(1);
+});
+
+let server: Server;
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string);
-    console.log('Database is connected successfully');
+    console.log('✅ Database is connected successfully');
 
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
+      console.log(`🙏 Application listening on port ${config.port}`);
     });
   } catch (error) {
-    console.log('Failed to connect database', error);
+    console.log('❌ Failed to connect database', error);
   }
+
+  // Gracefully shutting down the server in case of unhandled rejection
+  process.on('unhandledRejection', error => {
+    if (server) {
+      // Close the server and log the error
+      server.close(() => {
+        // errorLogger.error(error);
+        console.log(error);
+        process.exit(1);
+      });
+    } else {
+      // If server is not available, exit the process
+      process.exit(1);
+    }
+  });
 }
 
 bootstrap();
+
+/** SIGTERM
+ The SIGTERM signal is a standard signal in Unix-like operating systems that is used to request termination of a process. It is typically sent by the operating system or a process manager to gracefully stop a process.
+ This mechanism allows the process to perform any necessary cleanup or shutdown tasks before it exits, ensuring that resources are released properly and any pending operations are completed.
+ */
+process.on('SIGTERM', () => {
+  // logger.info('SIGTERM is received');
+  console.log('SIGTERM is received');
+  if (server) {
+    server.close();
+  }
+});
